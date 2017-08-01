53786786290f96b825eb0a7c6ced1a26c89c5fe5
Jennilyn Howell <jennilynhowell@me.com>
2017-08-01 08:40:01 -0400

    should use concrete class as dep injection in controller

--- src/main/java/com/jennilyn/controllers/HomeController.java
+++ src/main/java/com/jennilyn/controllers/HomeController.java
@@ -1,7 +1,7 @@
 package com.jennilyn.controllers;
 
-import com.jennilyn.interfaces.ProjectRepository;
 import com.jennilyn.models.Project;
+import com.jennilyn.repositories.ProjectRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,7 +15,7 @@ import java.util.List;
 public class HomeController {
 
     @Autowired
-    private ProjectRepository repo;
+    private ProjectRepositoryImpl repo;
 
     @RequestMapping("/")
     public String index(Model model){
